cc4a89c6bc2b3b8e8ec769a8c23c8d095475ae93
ravindradtechnoland <ravindrap@dtechnoland.com>
2025-10-27 13:37:56 +0530

    first commit 15

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://nextjsapp-3x7u.vercel.app/sitemap.xml",
+  };
+}
--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -1,14 +1,13 @@
-import { MetadataRoute } from 'next';
+export async function GET() {
+  const content = `
+User-agent: *
+Allow: /
+Sitemap: https://nextjsapp-3x7u.vercel.app/sitemap.xml
+`;
 
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        // disallow: '/admin', // Example: block admin pages
-      },
-    ],
-    sitemap: 'https://nextjsapp-3x7u.vercel.app/sitemap.xml',
-  };
+  return new Response(content, {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
 }
