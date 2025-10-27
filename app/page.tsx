6c0d27268feeda9edacf469697d1e2472e630a1d
ravindradtechnoland <ravindrap@dtechnoland.com>
2025-10-27 19:41:10 +0530

    first commit 18

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
           height={38}
           priority
         />
+        <Link href="/blog">Go to blog Page</Link>
+        <Link href="/dashboard">Go to dashboard Page</Link>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
