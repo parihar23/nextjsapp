4b969a7bd10e45a7823163107c8e886c51f995df
ravindradtechnoland <ravindrap@dtechnoland.com>
2025-10-27 13:32:35 +0530

    first commit 14

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: '/admin', // Example: block admin pages
+        // disallow: '/admin', // Example: block admin pages
       },
     ],
     sitemap: 'https://nextjsapp-3x7u.vercel.app/sitemap.xml',
