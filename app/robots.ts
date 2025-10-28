c7477352513068c83d111a7dd87a1f63a9469225
ravindradtechnoland <ravindrap@dtechnoland.com>
2025-10-28 11:09:32 +0530

    first commit 19

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://nextjsapp-3x7u.vercel.app/sitemap.xml",
+    sitemap: "https://indorefresh.site/sitemap.xml",
   };
 }
